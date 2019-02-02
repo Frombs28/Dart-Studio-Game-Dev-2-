763d1942d0e9f792167f86317b42fb0d7c7b7fb1
Anthony <anthonyveschi@gmail.com>
2019-02-02 17:21:07 -0500

    Input manager proof of concept what with talking to different characters

---
+++ Game Dev 2/Assets/Scripts/InputManagerScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManagerScript : MonoBehaviour
+{
+    public GameObject player;   //whoms't'd've is possessed rn?
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {player.SendMessage("Attack");}
+        else if (Input.GetMouseButtonDown(1)) {player.SendMessage("TraversalAbility");}
+    }
+}
